980aa64e1ba07d15123cc417959b9e605361e4d9
dudushy <eduardotalarico1337@gmail.com>
2022-07-21 20:50:32 -0300

    Make settings page

--- project/src/app/settings/settings.page.ts
+++ project/src/app/settings/settings.page.ts
@@ -8,6 +8,7 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  theme: boolean;
 
   constructor(
     private menu: MenuController
@@ -15,6 +16,8 @@ export class SettingsPage implements OnInit {
 
   ngOnInit() {
     console.log("init 'settings'");
+
+    this.loadTheme();
   }
 
   toggleMenu() {
@@ -22,28 +25,25 @@ export class SettingsPage implements OnInit {
     this.menu.toggle('mainMenu');
   }
 
-  // TODO
-  // loadTheme() {
-  //   if (localStorage.getItem("theme") == "true") {
-  //     this.theme = true;
-  //   } else {
-  //     this.theme = false;
-  //   }
-  // }
+  loadTheme() {
+    if (localStorage.getItem("theme") == "true") {
+      this.theme = true;
+    } else {
+      this.theme = false;
+    }
+  }
 
-  // TODO
-  // toggleTheme() {
-  //   console.log(this.theme);
-  //   if (this.theme) {
-  //     console.log("them: true");
-  //     localStorage.setItem("theme", "true");
-  //     document.body.setAttribute("color-theme", "dark");
-  //   } else {
-  //     console.log("theme: false");
-  //     localStorage.setItem("theme", "false");
-  //     document.body.setAttribute("color-theme", "light");
-  //   }
-  //   console.log("localStorage theme:", localStorage.getItem("theme"));
-  // }
-
+  toggleTheme() {
+    console.log(this.theme);
+    if (this.theme) {
+      console.log("them: true");
+      localStorage.setItem("theme", "true");
+      document.body.setAttribute("theme", "dark");
+    } else {
+      console.log("theme: false");
+      localStorage.setItem("theme", "false");
+      document.body.setAttribute("theme", "light");
+    }
+    console.log("localStorage theme:", localStorage.getItem("theme"));
+  }
 }
