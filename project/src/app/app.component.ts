ef89232f31a1eab4aaf0aadfa43ccd07d4a758e4
dudushy <eduardotalarico1337@gmail.com>
2022-07-21 19:37:44 -0300

    Making sidemenu

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { MenuController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  theme: boolean;
+  pagename: string;
+
+  constructor(
+    private router: Router,
+    private menu: MenuController
+  ) {
+    this.pagename = null;
+  }
+
+  ngOnInit() {
+    console.log("init 'app.component'");
+  }
+
+  redirectTo(url: string) {
+    console.log("redirect -> " + url);
+    this.router.navigateByUrl('/' + url);
+
+    this.pagename = url;
+  }
+
+  toggleMenu() {
+    this.menu.enable(true, 'mainMenu');
+    this.menu.toggle('mainMenu');
+  }
 }
