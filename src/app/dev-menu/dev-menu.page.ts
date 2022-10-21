563f5a2eb3fb3fb5fc7683d03bece4693de6dd25
dudushy <eduardotalarico1337@gmail.com>
2022-10-20 22:23:33 -0300

    Update dev-menu.page.ts

--- src/app/dev-menu/dev-menu.page.ts
+++ src/app/dev-menu/dev-menu.page.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { Platform } from '@ionic/angular';
+
+import { AppComponent } from '../app.component';
+
+import { StorageService } from 'src/app/.services/storage.service';
 
 @Component({
   selector: 'app-dev-menu',
@@ -6,10 +15,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dev-menu.page.scss'],
 })
 export class DevMenuPage implements OnInit {
+  title = 'DevMenuPage';
+  theme: any = 'dark';
 
-  constructor() { }
+  allPages: any = [];
 
-  ngOnInit() {
+  constructor(
+    public app: AppComponent,
+    public platform: Platform,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    public storage: StorageService
+  ) {
+    console.log(`[${this.title}#constructor]`);
   }
 
+  ngOnInit() {
+    console.log(`[${this.title}#ngOnInit]`);
+  }
+
+  ionViewDidEnter() {
+    this.platform.ready().then((readySource) => {
+      console.log(`[${this.title}#ionViewDidEnter] platform.ready`, readySource);
+
+      this.theme = this.storage.get('theme') == null ? 'dark' : this.storage.get('theme');
+
+      this.allPages = [];
+      const allRoutes = this.router.config;
+      for (const route of allRoutes) {
+        if (route.path !== '') {
+          if (route.path !== 'dev-menu') {
+            this.allPages.push(route.path);
+          }
+        }
+      }
+      console.log(`[${this.title}#ionViewDidEnter] allPages`, this.allPages);
+    });
+  }
+
+  defaultOrder() {
+    return 0;
+  }
+
+  updateView() {
+    console.log(`[${this.title}#updateView]`);
+    this.cdr.detectChanges();
+    this.app.updateView(this.title);
+  }
+
+  redirectTo(url: string) {
+    this.app.redirectTo(url, this.title);
+  }
 }
