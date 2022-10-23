6aa4de74864abd76b1d32f24e303ddc4a89bf2e6
dudushy <eduardotalarico1337@gmail.com>
2022-10-22 22:17:33 -0300

    Update storage.service.ts

--- src/app/.services/storage.service.ts
+++ src/app/.services/storage.service.ts
@@ -11,16 +11,13 @@ export class StorageService {
     console.log(`[${this.title}#constructor]`);
   }
 
-  set(varname: any, value: any) {
-    console.log(`[${this.title}#set] ${varname}`, value);
-    // console.log(`[${this.title}#set] JSON.stringify(value)`, JSON.stringify(value));
+  set(varname: any, value: any, from: any) {
+    console.log(`[${this.title}#set--${from}] ${varname}`, value);
     localStorage.setItem(varname, JSON.stringify(value));
   }
 
-  get(varname: any) {
-    console.log(`[${this.title}#get] varname`, varname);
-    // console.log(`[${this.title}#get] localStorage.getItem(varname)`, localStorage.getItem(varname));
-    // console.log(`[${this.title}#get] JSON.parse(varname)`, JSON.parse(localStorage.getItem(varname)));
+  get(varname: any, from: any) {
+    console.log(`[${this.title}#get--${from}] ${varname}`, JSON.parse(localStorage.getItem(varname)));
     return JSON.parse(localStorage.getItem(varname));
   }
 
